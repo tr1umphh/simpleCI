8caab954d98026c8b53cc337ae58d1190e99b5ba
Cong Thanh <83612550+tr1umphh@users.noreply.github.com>
2024-04-22 22:53:53 +0700

    Deleted cmake-single-platform.yml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include <fmt/color.h>
 #include <fmt/core.h>
 
 int main(int argc, char *argv[]) {
-  fmt::print("Hello world\n");
+  auto color_gold = fg(fmt::color::gold);
+  fmt::print(color_gold, "Hello world\n");
   return 0;
 }
