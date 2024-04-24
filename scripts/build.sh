2f308457fc1212fa20692d37b03379cd367791ad
Cong Thanh <83612550+tr1umphh@users.noreply.github.com>
2024-04-24 09:48:34 +0700

    Delete scripts directory

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export VCPKG_ROOT=/home/ubuntu/vcpkg
-export PATH=$VCPKG_ROOT:$PATH
-CXX=clang++-15 cmake --preset=default
-cmake --build build
--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-build/tests/unit_test
-build/pleaseBuildMe
